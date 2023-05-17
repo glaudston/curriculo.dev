2038997775e34d744703cd36067780eb810eaea4
Glaudston <glaudstonalan@gmail.com>
2023-05-17 18:17:17 -0300

    first commit

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
